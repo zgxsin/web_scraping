26b942079499291a7a7579200285e8600eb3b5c2
Guoxiang Zhou <harigxzhou@gmail.com>
2020-04-13 17:29:27 +0200

    Add helper functions

---
+++ helper_functions.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+
+def parse_single_url(url):
+    response_obj = requests.get(url)
+    soup_obj = BeautifulSoup(response_obj.text, 'lxml')
+    return soup_obj
+
+
+def print_pandas_data(pandas_data):
+    print (pandas_data.to_string())
